01173706c2e0f4f38bad430c800d246d0951b916
fedeagripa <federicogagripa@gmail.com>
2021-03-23 00:56:30 -0300

    add fighter videos

--- app/admin/fighter.rb
+++ app/admin/fighter.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register Fighter do
   permit_params :id, :email, :first_name, :last_name, :country, :reach, :height, :organization,
-                :birthdate, :cover_photo, :profile_pic, :preview_url, :sub_price, images: []
+                :birthdate, :cover_photo, :profile_pic, :preview_url, :sub_price, images: [],
+                public_videos: [], private_videos: []
 
 
   show do
@@ -14,16 +15,34 @@ ActiveAdmin.register Fighter do
       row :birthdate
       row :organization
       row "Profile Picture" do |p|
-        image_tag url_for(p.profile_pic)
+        image_tag(url_for(p.profile_pic), size: "200x200")
       end
       row "Cover Photo" do |p|
-        image_tag url_for(p.cover_photo)
+        image_tag(url_for(p.cover_photo), size: "200x200")
       end
       row "Images" do |p|
         ul do
           p.images.each do |photo|
             li do
-              image_tag url_for(photo)
+              image_tag(url_for(photo), size: "200x200")
+            end
+          end
+        end
+      end
+      row "Previews" do |p|
+        ul do
+          p.public_videos.each do |video|
+            li do
+              video_tag(url_for(video), size: "200x200")
+            end
+          end
+        end
+      end
+      row "Private content" do |p|
+        ul do
+          p.private_videos.each do |video|
+            li do
+              video_tag(url_for(video), size: "200x200")
             end
           end
         end
@@ -45,6 +64,12 @@ ActiveAdmin.register Fighter do
     f.input :profile_pic, as: :file
     f.inputs do
       f.input :images, as: :file, input_html: { multiple: true }
+    end
+    f.inputs do
+      f.input :public_videos, as: :file, input_html: { multiple: true }
+    end
+    f.inputs do
+      f.input :private_videos, as: :file, input_html: { multiple: true }
     end
     f.actions
   end
--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -15,6 +15,8 @@
 #
 class Fighter < ApplicationRecord
   has_many_attached :images
+  has_many_attached :public_videos
+  has_many_attached :private_videos
   has_one_attached :cover_photo
   has_one_attached :profile_pic
 end
