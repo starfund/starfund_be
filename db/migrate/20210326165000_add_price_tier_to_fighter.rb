77385fb740ff2f7fdd6d8d4b1419ab7251a570a3
fedeagripa <federicogagripa@gmail.com>
2021-03-26 17:37:33 -0300

    find or create price to be able to add column

--- db/migrate/20210326165000_add_price_tier_to_fighter.rb
+++ db/migrate/20210326165000_add_price_tier_to_fighter.rb
@@ -1,5 +1,6 @@
 class AddPriceTierToFighter < ActiveRecord::Migration[6.0]
   def change
+    PriceTier.find_or_create_by(level: 1)
     add_reference :fighters, :price_tier, null: false, foreign_key: true, default: 1
   end
 end
