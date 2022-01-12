a7b2022979ae96ccd83f35cf45a9a4cd22b9569e
fedeagripa <federicogagripa@gmail.com>
2022-01-12 16:32:55 -0300

    small details & precompile

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -12,16 +12,16 @@ ActiveAdmin.register_page 'Dashboard' do
         panel 'SUBSCRIPTIONS' do
           ul do
             last_subs = Subscription.active.where(created_at: 1.month.ago..)
-            h3 "Active Last Month - #{last_subs.count}"
+            h4 "Active Last Month - #{last_subs.count}"
             non_active_subs = Subscription.where.not(status: 0)
             cancel_last_month = non_active_subs.where(last_charge: 1.month.ago..)
-            h3 "Cancel Last Month - #{cancel_last_month.count}"
+            h4 "Cancel Last Month - #{cancel_last_month.count}"
             li link_to("See Subscriptions", admin_subscriptions_path)
             br
             br
             active_subs = Subscription.active.count
-            h3 "Total active subs - #{active_subs}"
-            h3 "Total cancel subs - #{non_active_subs.count}"
+            h4 "Total active subs - #{active_subs}"
+            h4 "Total cancel subs - #{non_active_subs.count}"
           end
         end
       end
@@ -30,16 +30,21 @@ ActiveAdmin.register_page 'Dashboard' do
           ul do
             last_subs = Subscription.where(last_charge_date: 1.month.ago..)
             last_charges = Charge.where(created_at: 1.month.ago..)
-            h3 "Eearnings Last Month SUBS - $#{last_subs.sum(:last_charge)/100}"
-            h3 "Eearnings Last Month PPVs - $#{last_charges.sum(:amount)/100}"
+            h4 "Eearnings Last Month SUBS - $#{last_subs.sum(:last_charge)/100}"
+            h4 "Eearnings Last Month PPVs - $#{last_charges.sum(:amount)/100}"
             li link_to("See Charges", admin_charges_path)
             br
             br
             total_e_subs = Subscription.all.sum(:last_charge)
             total_e_charges = Charge.all.sum(:amount)
-            h3 "Total earnings subs - $#{total_e_subs/100}"
-            h3 "Total earnings charges - $#{total_e_charges/100}"
-            h3 "Total earnings - $#{(total_e_subs + total_e_charges)/100}"
+            h4 "Total earnings subs - $#{total_e_subs/100}"
+            h4 "Total earnings charges - $#{total_e_charges/100}"
+            h4 "Total earnings - $#{(total_e_subs + total_e_charges)/100}"
+            br
+            br
+            h2 "SUMMARY"
+            h2 "AOW Earnings: $100"
+            h2 "STARFUND Amount: $20"
           end
         end
       end
